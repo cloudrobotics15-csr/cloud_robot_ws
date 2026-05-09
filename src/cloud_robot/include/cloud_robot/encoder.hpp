b42f80a3a0324dfc765454c8a1d8d6d1bd8850c9
cloudrobotics15-csr <cloudrobotics15@gmail.com>
2026-05-09 13:03:15 -0600

    Update encoder.hpp

--- src/cloud_robot/include/cloud_robot/encoder.hpp
+++ src/cloud_robot/include/cloud_robot/encoder.hpp
@@ -1,7 +1,7 @@
 #pragma once
+
 #include <atomic>
 #include <cstdint>
-#include <pigpiod_if2.h>
 
 namespace cloud_robot
 {
@@ -9,9 +9,8 @@ namespace cloud_robot
 class Encoder
 {
 public:
-
   Encoder(int pi, int pin_a, int pin_b);
-  
+
   void update();
 
   int32_t get_ticks() const;
@@ -19,18 +18,14 @@ public:
   void reset();
 
 private:
-
   int pi_;
 
   int pin_a_;
   int pin_b_;
 
   int last_a_;
-  int last_b_;
 
   std::atomic<int32_t> ticks_;
 };
 
-}// namespace cloud_robot
-
-
+} // namespace cloud_robot
