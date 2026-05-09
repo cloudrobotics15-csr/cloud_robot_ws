4cfed8a51b5123fc1009fce3a5ac83a33147e628
cloudrobotics15-csr <cloudrobotics15@gmail.com>
2026-05-09 13:07:43 -0600

    Update encoder.cpp

--- src/cloud_robot/src/encoder.cpp
+++ src/cloud_robot/src/encoder.cpp
@@ -1,53 +1,48 @@
 #include "cloud_robot/encoder.hpp"
+#include <pigpiod_if2.h>
 
 namespace cloud_robot
 {
 
 Encoder::Encoder(int pi, int pin_a, int pin_b)
-: pi_(pi),
-  pin_a_(pin_a),
-  pin_b_(pin_b),
-  last_a_(0),
-  last_b_(0),
-  ticks_(0)
+: pi_(pi), pin_a_(pin_a), pin_b_(pin_b), ticks_(0)
 {
-  set_mode(pi_, pin_a_, PI_INPUT);
-  set_mode(pi_, pin_b_, PI_INPUT);
+    set_mode(pi_, pin_a_, PI_INPUT);
+    set_mode(pi_, pin_b_, PI_INPUT);
 
-  set_pull_up_down(pi_, pin_a_, PI_PUD_UP);
-  set_pull_up_down(pi_, pin_b_, PI_PUD_UP);
+    set_pull_up_down(pi_, pin_a_, PI_PUD_UP);
+    set_pull_up_down(pi_, pin_b_, PI_PUD_UP);
 
-  last_a_ = gpio_read(pi_, pin_a_);
-  last_b_ = gpio_read(pi_, pin_b_);
+    last_a_ = gpio_read(pi_, pin_a_);
+}
 
+int32_t Encoder::get_ticks() const
+{
+    return ticks_.load(std::memory_order_relaxed);
 }
 
 void Encoder::update()
 {
-  int a = gpio_read(pi_, pin_a_);
-  int b = gpio_read(pi_, pin_b_);
+    int a = gpio_read(pi_, pin_a_);
 
-  if (a == last_a_ && b == last_b_)
-    return;
+    // solo si A cambia → contamos (x2)
+    if (a != last_a_)
+    {
+        int b = gpio_read(pi_, pin_b_);
 
-  if (a == b)
-    ticks_++;
-  else
-    ticks_--;
+        // dirección
+        if (b != a)
+            ticks_++;
+        else
+            ticks_--;
+    }
 
-  last_a_ = a;
-  last_b_ = b;
+    last_a_ = a;
 }
- 
-int32_t Encoder::get_ticks() const
-{
-  return ticks_.load();
-}
-
 
 void Encoder::reset()
 {
-  ticks_.store(0);
+    ticks_.store(0);
 }
 
-}// namespace cloud_robot
+} // namespace cloud_robot
